f6bb47972769c9d3202505beba1487354c2ed59e
SJS <sjsevadjian@gmail.com>
2020-08-01 20:03:50 -0700

    initializing repo

---
+++ javascript.js
@@ -0,0 +1,116 @@
+// Assignment Code
+const generateBtn = document.querySelector("#generate");
+const specialCharacter = ["@", "$", "%", "^", "!", "#", "*", "`", "(", ")", "[", "]", "{", "+", "=", "-"];
+
+// Write password to the #password input
+function writePassword() {
+  const password = generatePassword();
+  const passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+function generatePassword() {
+  let passwordLength = prompt("Please enter the desired length of your password. This must be at least 8 characters long, but less than 128 characters.");
+  if (passwordLength < 8 || passwordLength > 128) {
+    alert("Must be at least 8 characters and less than 128.");
+    return;
+  }
+  if (isNaN(passwordLength) === true) {
+    alert("Password length must be provided as a number.");
+    return;
+  }
+
+  let lowercaseConfirm = confirm("Would you like to include lower case characters?");
+
+  let uppercaseConfirm = confirm("Would you like to include uppercase characters?");
+
+  let numericConfirm = confirm("Would you like to include numbers in your password?");
+
+  let specialCharactersConfirm = confirm("Would you like to include special characters?");
+
+  if (lowercaseConfirm === false && uppercaseConfirm === false && numericConfirm === false && specialCharactersConfirm === false) {
+    alert("You must select at least one character type.");
+    return;
+  }
+
+  let minCharacters = 0;
+
+  let lowercase = "";
+  let uppercase = "";
+  let numeric = "";
+  let specialCharacters = "";
+
+  let passwordCreator = {
+    getLowercase: function () {
+      return String.fromCharCode(Math.floor(Math.random() * 26 + 97));
+    },
+    getUppercase: function () {
+      return String.fromCharCode(Math.floor(Math.random() * 26 + 65));
+    },
+    getNumeric: function () {
+      return String.fromCharCode(Math.floor(Math.random() * 10 + 48));
+    },
+    getSpecialCharacters: function () {
+      let randomIndex = Math.floor(Math.random() * specialCharacter.length);
+      console.log("randomIndex is" + randomIndex);
+      let randomElement = specialCharacter[randomIndex];
+      console.log("my randomElement" + randomElement);
+      return randomElement;
+    }
+  };
+  /*let options = hasGetPasswordOptions();
+  function hasGetPasswordOptions () {
+
+  let lowercaseConfirm = confirm("Would you like to include lower case characters?");
+   
+  let uppercaseConfirm = confirm("Would you like to include uppercase characters?");
+
+  let numericConfirm = confirm("Would you like to include numbers in your password?");
+
+  let specialCharactersConfirm = confirm("Would you like to include special characters?");
+
+    if (lowercaseConfirm === false && uppercaseConfirm === false && numericConfirm === false && specialCharactersConfirm === false) {
+      alert("You must select at least one character type.");
+      return;
+    }
+} */
+
+  if (lowercaseConfirm === true) {
+    lowercase = passwordCreator.getLowercase();
+    minCharacters++;
+  }
+
+  if (uppercaseConfirm === true) {
+    uppercase = passwordCreator.getUppercase();
+    minCharacters++;
+  }
+
+  if (numericConfirm === true) {
+    numeric = passwordCreator.getNumeric();
+    minCharacters++;
+  }
+
+  if (specialCharactersConfirm === true) {
+    specialCharacters = passwordCreator.getSpecialCharacters();
+    minCharacters++;
+  }
+
+  let myRandomPassword = "";
+  for (let i = 0; i < (parseInt(passwordLength) - minCharacters); i++) {
+    let randomNumberPicked = Math.floor(Math.random() * 4);
+    console.log("my random number" + randomNumberPicked);
+    myRandomPassword += randomNumberPicked;
+  }
+  myRandomPassword += lowercase;
+  myRandomPassword += uppercase;
+  myRandomPassword += numeric;
+  myRandomPassword += specialCharacters;
+
+  return myRandomPassword;
+
+}
